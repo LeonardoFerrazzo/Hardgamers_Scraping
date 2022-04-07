81861bb1a0a8355fa47d5b14af879a4ba51662ef
LeonardoFerrazzo <98769952+LeonardoFerrazzo@users.noreply.github.com>
2022-04-07 10:54:46 -0300

    Update Hardgamers.py
    Minor bug fixes - LLF

--- Hardgamers.py
+++ Hardgamers.py
@@ -9,20 +9,12 @@ componente = []
 
 while len(componente)<qprod:
     parte = input('Ingrese componente a buscar ')
-    componente.append(parte) #El Append se usa para agregar elementos a la lista
-
-#for i in range (qprod):
- #   componente.append(i)=input('Ingrese producto a buscar')
- #   i=0
+    componente.append(parte)
 
 gpu = input('Ingrese producto a buscar. Ej: "3070" para buscar una RTX 3070, Use "+" para seguir agregando productos. Ej: 3070+5600x')
 url = 'https://www.hardgamers.com.ar/search?text='+gpu
-page = requests.get(url) #Pido el request a la url que defini antes
-soup = BeautifulSoup(page.content,'html5lib') #El lxml será mi parser de html
-
-#print(soup.prettify()) #Veo el código html.
-
-#page = soup.find()
+page = requests.get(url)
+soup = BeautifulSoup(page.content,'html5lib')
 
 print("--------------------------------------------------------------------")
 
@@ -31,7 +23,7 @@ print("Precio de componente")
 for product_price in soup.find_all('h2',class_='product-price'):
    precio = product_price.text
    print(precio)
-   lprecio=soup.find_all('h2',class_='product-price') #Creo una lista con los valores
+   lprecio=soup.find_all('h2',class_='product-price')
 
 print("--------------------------------------------------------------------")
 
